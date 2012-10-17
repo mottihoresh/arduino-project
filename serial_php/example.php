8e13fff89e2c87b4621119dca16a90c8832fd027
Motti Horesh <motti@horesh-studios.com>
2012-10-17 00:15:00 -0400

    added serial classes

--- serial_php/example.php
+++ serial_php/example.php
@@ -6,10 +6,10 @@ $serial = new phpSerial;
 
 // First we must specify the device. This works on both linux and windows (if
 // your linux serial device is /dev/ttyS0 for COM1, etc)
-$serial->deviceSet("COM1");
+$serial->deviceSet("/dev/ttyUSB");
 
 // We can change the baud rate, parity, length, stop bits, flow control
-$serial->confBaudRate(2400);
+$serial->confBaudRate(9600);
 $serial->confParity("none");
 $serial->confCharacterLength(8);
 $serial->confStopBits(1);
@@ -19,16 +19,13 @@ $serial->confFlowControl("none");
 $serial->deviceOpen();
 
 // To write into
-$serial->sendMessage("Hello !");
+//$serial->sendMessage("Hello !");
 
 // Or to read from
 $read = $serial->readPort();
 
 // If you want to change the configuration, the device must be closed
-$serial->deviceClose();
-
-// We can change the baud rate
-$serial->confBaudRate(2400);
+ echo $read;
 
 // etc...
 //
